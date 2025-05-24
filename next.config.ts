64fe9b0802abe7893deb1413b13faa2664f6c560
Rifai-13 <muhrifai130604@gmail.com>
2025-05-24 13:14:12 +0700

    update next

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,25 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   env: {
     MONGO_URI: process.env.MONGO_URI,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
+   // Redirect HTTP ke HTTPS
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
+  // Optional: Jika hosting di Vercel, tambahkan ini
+  trailingSlash: true,
   
 };
 
